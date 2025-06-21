2570082818e14f46898b7faaa47fd050a39ee4d7
Thiago-Landi <thiagolandi0902@gmail.com>
2025-06-21 08:25:42 -0300

    Specialty delete and update

--- src/main/java/com/Thiago_Landi/medical_clinic/controller/SpecialtyController.java
+++ src/main/java/com/Thiago_Landi/medical_clinic/controller/SpecialtyController.java
@@ -1,11 +1,14 @@
 package com.Thiago_Landi.medical_clinic.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,5 +39,31 @@ public class SpecialtyController {
 	public ResponseEntity<List<Specialty>> findByTitle(@PathVariable String title){
 		List<Specialty> specialties = specialtyService.findByTitle(title);
 		return ResponseEntity.ok(specialties);
+	}
+	
+	@DeleteMapping("{id}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Void> delete(@PathVariable("id") String id) {
+		Optional<Specialty> speciaOptional = specialtyService.findById(id);
+		if(speciaOptional.isEmpty()) return ResponseEntity.notFound().build();
+		
+		specialtyService.delete(speciaOptional.get());
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PatchMapping("{id}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Void> update(@PathVariable("id") String id, @RequestBody SpecialtyDTO dto) {
+		Long longId; 
+		try {
+		        longId = Long.parseLong(id);
+		        if(specialtyService.findById(id).isEmpty()) return ResponseEntity.notFound().build();
+		    } catch (NumberFormatException e) {
+		        throw new IllegalArgumentException("ID inválido: precisa ser um número");
+		    }
+		 
+		 specialtyService.update(longId, dto);
+		 return ResponseEntity.noContent().build();
+		
 	}
 }
--- src/main/java/com/Thiago_Landi/medical_clinic/service/SpecialtyService.java
+++ src/main/java/com/Thiago_Landi/medical_clinic/service/SpecialtyService.java
@@ -1,6 +1,7 @@
 package com.Thiago_Landi.medical_clinic.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -28,5 +29,32 @@ public class SpecialtyService {
 		if (specialties.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhuma especialidade encontrada");
 		
 		return specialties;
+	}
+	
+	public void delete(Specialty specialty) {
+		specialtyRepository.delete(specialty);
+	} 
+	
+	public void update(Long id, SpecialtyDTO dto) {
+		Specialty model = specialtyRepository.findById(id).orElseThrow(
+				() -> new IllegalArgumentException("The specialty with the provided ID does not exist in the bank."));
+		
+		updateData(model, dto);
+		specialtyRepository.save(model);
+	}
+	
+	private void updateData(Specialty model, SpecialtyDTO dto) {
+		if(dto.title() != null) model.setTitle(dto.title());
+		
+		if(dto.description() != null) model.setDescription(dto.description());
+	}
+
+	public Optional<Specialty> findById(String id) {
+		 try {
+		        Long longId = Long.parseLong(id);
+		        return specialtyRepository.findById(longId);
+		    } catch (NumberFormatException e) {
+		        throw new IllegalArgumentException("ID inválido");
+		    }
 	}
 }
